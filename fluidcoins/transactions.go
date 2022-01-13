fe7bfb9183d3d8632e3768f6c8d12bcd08c99224
Lanre Adelowo <yo@lanre.wtf>
2022-01-13 02:05:24 +0100

    [MERS-155] add customers,transaction and merchant models

--- fluidcoins/transactions.go
+++ fluidcoins/transactions.go
@@ -34,9 +34,8 @@ type Merchant struct {
 
 	CountryID uuid.UUID `json:"country_id"`
 
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"-" pg:",soft_delete"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 // GetByID fetches a transaction.
